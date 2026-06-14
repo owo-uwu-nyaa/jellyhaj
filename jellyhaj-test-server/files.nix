96980d15b2558c087cdcf181fb14f8689413fb46
Robin Marchart <robin.marchart@gmail.com>
2026-06-14 22:18:35 +0200

    use toString on port

--- jellyhaj-test-server/files.nix
+++ jellyhaj-test-server/files.nix
@@ -1,7 +1,7 @@
 { pkgs, port }:
 pkgs.runCommand "jellyhaj-test-server-files" {} ''
   mkdir $out
-  substitute ${./network.xml} $out/network.xml --replace-fail "@port@" ${port}
-  substitute ${./setup-jellyfin.sh} $out/setup-jellyfin --replace-fail "@port@" ${port}
+  substitute ${./network.xml} $out/network.xml --replace-fail "@port@" ${toString port}
+  substitute ${./setup-jellyfin.sh} $out/setup-jellyfin --replace-fail "@port@" ${toString port}
   chmod +x $out/setup-jellyfin
 ''
