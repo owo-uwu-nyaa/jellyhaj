ecb73e6e026819fae998c1c3d526b9ef764f9b5a
Robin Marchart <robin.marchart@gmail.com>
2026-06-14 22:06:35 +0200

    fix runCommand usage

--- jellyhaj-test-server/files.nix
+++ jellyhaj-test-server/files.nix
@@ -1,5 +1,5 @@
 { pkgs, port }:
-pkgs.runCommand "jellyhaj-test-server-files" ''
+pkgs.runCommand "jellyhaj-test-server-files" {} ''
   mkdir $out
   substitute ${./network.xml} $out/network.xml --replace-fail "@port@" ${port}
   substitute ${./setup-jellyfin.sh} $out/setup-jellyfin --replace-fail "@port@" ${port}
