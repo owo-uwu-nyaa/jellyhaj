78d9c428b74d4c57d63338df7c7e7b4c96b2b148
Robin Marchart <robin.marchart@gmail.com>
2026-06-15 16:43:36 +0200

    trs sending redundant remote access configuration

--- jellyhaj-test-server/setup-jellyfin.sh
+++ jellyhaj-test-server/setup-jellyfin.sh
@@ -2,18 +2,27 @@
 
 set -e
 set -u
+echo sending general configuration
 curl --fail-with-body -X 'POST' 'http://localhost:@port@/Startup/Configuration' -H 'accept: */*' -H 'Content-Type: application/json' \
+    -H 'authorization: MediaBrowser Client="curl",Device="curl",DeviceId="42",Version="1"' \
     -d '{
     "MetadataCountryCode": "US",
     "PreferredMetadataLanguage": "en",
     "ServerName": "jellyhaj-test-server",
     "UICulture": "en-US"
 }'
-
+echo sending user information
 curl --fail-with-body -X 'POST' 'http://localhost:@port@/Startup/User' -H 'accept: */*' -H 'Content-Type: application/json' \
+    -H 'authorization: MediaBrowser Client="curl",Device="curl",DeviceId="42",Version="1"' \
   -d '{
   "Name": "jellyfin",
   "Password": "jellyfin"
 }'
-
-curl --fail-with-body -X 'POST' 'http://localhost:@port@/Startup/Complete' -H 'accept: */*' -d ''
+echo setting remote access
+curl -X 'POST' 'http://localhost:@port@/Startup/RemoteAccess' -H 'accept: */*' -H 'Content-Type: application/json' \
+    -H 'authorization: MediaBrowser Client="curl",Device="curl",DeviceId="42",Version="1"' \
+    -d '{"EnableRemoteAccess": false}'
+echo completing setup
+curl --fail-with-body -X 'POST' 'http://localhost:@port@/Startup/Complete' -H 'accept: */*' \
+    -H 'authorization: MediaBrowser Client="curl",Device="curl",DeviceId="42",Version="1"' \
+    -d ''
